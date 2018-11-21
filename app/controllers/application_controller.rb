3a1e4cd1e94e293b5dfacf975eb54ecc599c9293
Mani <rm3970@gmail.com>
2018-11-21 14:17:41 +0530

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
 protect_from_forgery with: :exception
 
-  def hello
-    render html: "hello, world!"
-  end
+Rails.application.routes.draw do
+  resources :users
+  root 'users#index'
+end
+
+Rails.application.routes.draw do
+  resources :microposts
+  root 'microposts#index'
+end
+
 
 end
